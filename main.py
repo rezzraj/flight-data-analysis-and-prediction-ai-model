5f51b30a01a288e4443112c96984a6225379c37f
rezzraj <akshitaviraj@gmail.com>
2025-07-31 16:46:05 +0530

    more analysis

--- main.py
+++ main.py
@@ -2,17 +2,36 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-pd.set_option('display.max_columns', None)
 
-df=pd.read_csv('C:\\Users\\akshi\\Downloads\\all stuff\\airlines_flights_data.csv')
+df=pd.read_csv('airlines_flights_data.csv')
 
-sns.scatterplot(x='airline', y='duration', data=df)
+#comparing different values by plotting graphs
+
+
+#ariline vs duration
+sns.barplot(x='airline', y='duration', data=df)
 plt.show()
 
-
+#duration vs price
 sns.scatterplot(data=df,x='duration',y='price')
 plt.show()
 
-
+#airline vs price
 sns.barplot(data=df, x='airline',y='price')
 plt.show()
+
+#airline vs no of flights
+airline_counts= df['airline'].value_counts().reset_index()
+airline_counts.columns=['airline','flight_count']
+
+#no of flights for all airlines
+print(airline_counts)
+sns.barplot(data=airline_counts, x=airline_counts.airline, y=airline_counts.flight_count)
+plt.show()
+
+#which airline has the longest flight
+max_duration=df['duration'].max()
+max_duration_row=df[df['duration']==max_duration]
+
+
+print(max_duration_row.iloc[0]['airline'])
