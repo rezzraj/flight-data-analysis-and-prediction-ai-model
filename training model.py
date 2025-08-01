f85131f9b0b4b7574cbce9f725c20e4c2fd00969
rezzraj <akshitaviraj@gmail.com>
2025-08-02 01:26:39 +0530

    added visualization in diff file

--- training model.py
+++ training model.py
@@ -7,45 +7,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score,mean_absolute_error
 from sklearn.linear_model import LinearRegression
 
+
 df=pd.read_csv('airlines_flights_data.csv')
 df=df.dropna()
-
-
-
-#total number of cities
-print(f'No of cities being analysed {df['source_city'].unique()}')
-#comparing different values by plotting graphs
-#ariline vs duration
-max_dur=df.groupby('airline')['duration'].max().reset_index()
-sns.barplot(x='airline', y='duration', data=max_dur)
-plt.title('Airline Vs Duration ', fontsize='16', fontweight='bold')
-plt.xlabel('all airlines')
-plt.ylabel('duration in hours')
-plt.show()
-#duration vs price
-grouped=df.groupby('duration')['price'].mean().reset_index()
-sns.histplot(data=grouped, x= 'duration', y='price')
-plt.show()
-
-#airline vs price
-sns.barplot(data=df, x='airline',y='price')
-plt.show()
-
-#no of flights for all airlines
-sns.countplot(data=df, x= 'airline')
-plt.show()
-
-#which airline has the longest flight
-max_duration=df['duration'].max()
-max_duration_row=df[df['duration']==max_duration]
-print(f'{max_duration_row.iloc[0]['airline']} Had the longest flight')
-
-#max value for money
-df['value']=df['duration']/df['price']
-effi=df.groupby('airline')['price'].mean().sort_values().reset_index()
-print(f'Avg Price of each airline \n{effi}')
-most_value_for_money=effi.loc[0,'airline']
-print(f'{most_value_for_money} has the best value for money')
 
 #training the Random forest regressor model
 
